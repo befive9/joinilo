db95290e7c296902dcce5008ee9674fe747eb37e
befive9 <coolwhipisthebest@gmail.com>
2025-07-09 21:12:48 +0900

    docs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/joinilo/',     // ← 이 한 줄 추가!
+  base: '/joinilo/',         // ← 이미 추가함!
+  build: {
+    outDir: 'docs',          // ← 이거만 더 추가!
+  },
 })
