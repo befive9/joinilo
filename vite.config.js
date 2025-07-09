2257155a3d5bf789c446954c0f7271d74ec41644
befive9 <coolwhipisthebest@gmail.com>
2025-07-09 23:54:50 +0900

    domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/joinilo/',         // ← 이미 추가함!
+  base: '/',         // ← 이미 추가함!
   build: {
     outDir: 'docs',          // ← 이거만 더 추가!
   },
